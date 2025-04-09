17f758dd3a01e0bf18fe13a38f10dbbfff2c14db
kamitsui <kamitsui@student.42tokyo.jp>
2025-04-09 17:16:41 +0900

    Fix cin.cpp

--- std/cin.cpp
+++ std/cin.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
+#include <limits>
+#include <ios> // For std::streamsize
 
-// 入出力ストリーム
-int main()
-{
-	int	number;
-	std::cout << "Enter a number: ";
-	//std::cin >> number;
-	if ((std::cin >> number) != NULL)
-	{
-		std::cout << "Invalid input. Please enter a number." << std::endl;
-		std::cin.clear();// Clear the error state
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
-		return 1;
-	};
-	std::cout << "You entered: " << number << std::endl;
+void check(bool ret) {
+	std::cout << "--- check() ---" << std::endl;
+	std::cout << "std:cin:" << ret << std::endl;
+	std::cout << "fail() :" << std::cin.fail() << std::endl;
+	std::cout << "bad()  :" << std::cin.bad() << std::endl;
+	std::cout << "good() :" << std::cin.good() << std::endl;
+	std::cout << "---------------" << std::endl;
+}
+
+int main() {
+	int	age;
+	//while (true) {
+	//	std::cout << "Enter your age (an integer): ";
+	//	if (std::cin >> age) {
+	//		check(true);
+	//		// Success full
+	//		break;
+	//	} else {
+	//		check(false);
+	//		std::cerr << "Invalid input. Please enter an integer." << std::endl;
+	//		std::cin.clear();// Clear the error state
+	//		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+	//		//return 1;
+	//	}
+	//}
+	//std::cout << "You age is: " << age << std::endl;
+
+	std::cout << "1th Enter your age (an integer): ";
+	while (std::cin >> age) {
+		std::cout << "You age is: " << age << std::endl;
+	}
+	std::cout << "2th Enter your age (an integer): ";
+	while (std::cin >> age) {
+		std::cout << "You age is: " << age << std::endl;
+	}
+
+	//std::cin.clear();// Clear the error state
+	//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+
 
 	char	str[10];
 	std::cout << "Enter a string: ";
